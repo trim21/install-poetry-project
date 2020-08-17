c1bb3c21d7f89b285716330f3628998088cc3a29
Trim21 <i@trim21.me>
2020-08-18 04:06:03 +0800

    reduce log

--- src/cache.ts
+++ src/cache.ts
@@ -5,9 +5,7 @@ import { PYTHONUSERBASE } from './constants'
 import { hashString } from './utils'
 
 function cacheKey (pyVersion: string, extras: string[]): string {
-  const key = `poetry-deps-1-${process.platform}-${hashString(pyVersion)}-${poetryLockCacheKey()}-${hashString(extras.join('_'))}`
-  core.info(`cache with key ${key}`)
-  return key
+  return `poetry-deps-1-${process.platform}-${hashString(pyVersion)}-${poetryLockCacheKey()}-${hashString(extras.join('_'))}`
 }
 
 function poetryLockCacheKey () {
@@ -19,9 +17,11 @@ export async function setup (
   extras: string[]
 ): Promise<void> {
   try {
+    const key = cacheKey(pythonVersion, extras)
+    core.info(`cache with key ${key}`)
     await cache.saveCache(
       [PYTHONUSERBASE],
-      cacheKey(pythonVersion, extras)
+      key
     )
   } catch (e) {
     if (e.toString().includes('reserveCache failed')) {
@@ -36,9 +36,15 @@ export async function restore (
   pythonVersion: string,
   extras: string[]
 ): Promise<Boolean> {
+
+  const primaryKey = cacheKey(pythonVersion, extras)
+  const fallbackKeys = [cacheKey(pythonVersion, [])]
+  core.info(`restore cache with key ${primaryKey}`)
+  core.info(`fallback to ${fallbackKeys}`)
+
   return !!(await cache.restoreCache(
     [PYTHONUSERBASE],
-    cacheKey(pythonVersion, extras),
-    [cacheKey(pythonVersion, [])]
+    primaryKey,
+    fallbackKeys
   ))
 }
